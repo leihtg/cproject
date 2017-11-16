fdc5cf12a4e084b44ba5da3f5adbe802f36dbab4
leihtg <leihtg@163.com>
2017-11-16 12:03:35 +0800

    update

--- C语言学习/consoleMsg/consoleMsg/main.cpp
+++ C语言学习/consoleMsg/consoleMsg/main.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 int maina(){
-	RegisterHotKey(NULL,1,NULL,'M');
-	RegisterHotKey(NULL,2,NULL,'C');
+	RegisterHotKey(NULL, 1, NULL, 'M');
+	RegisterHotKey(NULL, 2, NULL, 'C');
 	MSG msg;
-	while(GetMessage(&msg,NULL,NULL,NULL)){
-		HWND hnd=WindowFromPoint(msg.pt);
+	while (GetMessage(&msg, NULL, NULL, NULL)){
+		HWND hnd = WindowFromPoint(msg.pt);
 		char buf[100];
-		GetClassName(hnd,buf,100);
-		cout<<buf<<endl;
-		cout<<hnd<<endl;
-		if(msg.wParam == 2){
+		GetClassName(hnd, buf, 100);
+		cout << buf << endl;
+		cout << hnd << endl;
+		if (msg.wParam == 2){
 			//PostMessage(hnd,WM_CLOSE,NULL,NULL);
 		}
 	}
@@ -26,24 +26,24 @@ HANDLE hOut;
 HANDLE hIn;
 
 void main()
-{	
+{
 	INPUT_RECORD mouseRec;
 	DWORD state = 0, res;
 	COORD pos;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE); // 获取标准输出设备句柄
 	hIn = GetStdHandle(STD_INPUT_HANDLE); // 获取标准输入设备句柄
-	while(1)
+	while (1)
 	{
 
 		//printf("123");	
 		ReadConsoleInput(hIn, &mouseRec, 1, &res);
-		if (mouseRec.EventType==MOUSE_EVENT)
+		if (mouseRec.EventType == MOUSE_EVENT)
 		{
-			if (mouseRec.Event.MouseEvent.dwEventFlags==MOUSE_MOVED)
+			if (mouseRec.Event.MouseEvent.dwEventFlags == MOUSE_MOVED)
 			{
-				pos=mouseRec.Event.MouseEvent.dwMousePosition;
-				printf("(%d,%d)\n",pos.X,pos.Y);
-			}	
+				pos = mouseRec.Event.MouseEvent.dwMousePosition;
+				printf("(%d,%d)\n", pos.X, pos.Y);
+			}
 			//SetCursorPos(
 		}
 	}
--- C语言学习/练习/main.cpp
+++ C语言学习/练习/main.cpp
@@ -1,47 +1,50 @@
 #include <windows.h>
-#include <iostream.h>
-USHORT CheckSum(USHORT *buffer,int size){
-	unsigned long cksum=0;
-	while(size>=1){
-		cksum+=*buffer++;
-		size-=sizeof(USHORT);
+#include <iostream>
+using namespace std;
+
+USHORT CheckSum(USHORT *buffer, int size){
+	unsigned long cksum = 0;
+	while (size >= 1){
+		cksum += *buffer++;
+		size -= sizeof(USHORT);
 	}
-	if(size){
-		cksum+=*(UCHAR*)buffer;
+	if (size){
+		cksum += *(UCHAR*)buffer;
 	}
-	cksum=(cksum>>16)+(cksum&0xffff);
-	cksum+=(cksum>>16);
+	cksum = (cksum >> 16) + (cksum & 0xffff);
+	cksum += (cksum >> 16);
 	return (USHORT)(~cksum);
 }
 class Z;
 class A{
 public:
-	put(Z*);
+	void put(Z*);
 };
 class Z{
+private:
 	int i;
 public:
 	Z(int i){
-		this->i=i;
+		this->i = i;
 	}
 	void print(){
-		cout<<"i is "<<i<<endl;
+		cout << "i is " << i << endl;
 	}
+	friend class A;// 只有你赋予某个类为你的友元时，那个类才有访问你的私有数据的权利。
 	friend void put(Z*);
-	friend  A::put(Z*);
+	friend void A::put(Z*);
 };
-A::put(Z* z){
-	z->i=20;
+void A::put(Z* z){
+	z->i = 20;
 }
 void put(Z* z){
-	z->i=10;
+	z->i = 10;
 }
 int main(){
-	char ch[]="1";
-	cout<<CheckSum((USHORT*)ch,sizeof(ch))<<endl;
-/*
-	cout<<endl;
-	cout<<sizeof(USHORT)<<endl;
+	char ch[] = "178asdfasd";
+	cout << CheckSum((USHORT*)ch, sizeof(ch)) << endl;
+	cout << endl;
+	cout << sizeof(USHORT) << endl;
 	Z z(1);
 	z.print();
 	put(&z);
@@ -49,6 +52,5 @@ int main(){
 	A a;
 	a.put(&z);
 	z.print();
-	*/
 	return 0;
 }
